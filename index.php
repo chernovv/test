19720f8839f44e1a84024a263f9caf3a39aff3c7
Vasiliy Chernov <vchernov@geeksforless.net>
2014-01-22 21:34:25 +0200

    Commit all and delete that not used

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
+<!doctype html>
 <html>
+<head>    
 <link type="text/css" href="lib/style.css" rel="stylesheet"/>
+</head>
 <body>
     <iframe src="lib/hello.html" width="200" height="200" />
 </body>
