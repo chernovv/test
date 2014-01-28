510d1757a854f8aea90f3e142135e172ce337dfb
Vasiliy Chernov <vchernov@geeksforless.net>
2014-01-28 22:05:48 +0200

    create test -BLOG

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'e2b089953841aa3f4acca873cdeb6cfcd40c565c76b9ec116cfd9584f86bc9e7e9fe2da20a894ab0e38bd614129872d6ad77ab847fce1ad53eec759153df6a5c'
